3d91db91140b9ca76cf299316cba8727dffb3fb9
unknown <devskyworth03@gmail.com>
2026-05-01 00:19:30 +0800

    fix: fixed settings

--- truck_backend/settings.py
+++ truck_backend/settings.py
@@ -18,7 +18,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     "corsheaders",
     "rest_framework",
     "planner",
